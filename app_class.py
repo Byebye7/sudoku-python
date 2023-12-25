ce7cd8b7429b4a4eb0adfb388df4f1a2bb56642c
Byebye7 <brywang@hotmail.com>
2023-12-25 14:46:46 -0500

    update

--- app_class.py
+++ app_class.py
@@ -1,21 +1,31 @@
 import pygame
+import random
+import pickle
 import sys
 from settings import *
 from buttonClass import *
 from sudokusolve import *
+import requests
+from bs4 import BeautifulSoup
 
 class App:
 	def __init__(self):
 		pygame.init()
 		self.window = pygame.display.set_mode((WIDTH,HEIGHT))
+		pygame.display.set_caption("Sudoku!")
 		self.hintCounter = 0
 		self.running = True
 		self.puzzleComplete = False
-		self.grid = testBoard2
+		self.difficulty = 1
+		#self.grid = testBoard2
+		self.timer = "00:00:00"
+		self.puzzlenum = 0
+		self.init_timer = 0
 		self.selected = None
 		self.mousePos = None
-		self.state = "playing"
+		self.state = "newgame"
 		self.playingButtons = []
+		self.newgameButtons = []
 		self.menuButtons = []
 		self.endButtons = []
 		self.lockedCells = []
@@ -24,13 +34,9 @@ class App:
 		self.cellChanged = False
 		self.solvedGrid = [[0 for x in range(9)] for x in range(9)]
 		# copy original puzzle to solvedGrid
-		for idx,row in enumerate(self.grid):
-			self.solvedGrid[idx]= row.copy()
-		self.solve()
-		for row in self.solvedGrid:
-			print(row)
 
-		self.load()
+		self.loadButtons()
+		#self.loadGame()
 		self.font = pygame.font.SysFont("arial",cellSize//2)
 
 
@@ -40,9 +46,48 @@ class App:
 				self.playing_events()
 				self.playing_update()
 				self.playing_draw()
+			elif self.state == "newgame":
+				self.newgame_events()
+				self.newgame_update()
+				self.newgame_draw()
 
 		pygame.quit()
 		sys.exit()
+
+############### NEW GAME FUNCTIONS #############
+	def newgame_events(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.running = False
+
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				self.mousePos = pygame.mouse.get_pos()
+				#check to see if pushed any buttons
+				for button in self.newgameButtons:
+					if button.pressed(self.mousePos):
+						button.function(button.params)
+
+			elif event.type == pygame.KEYDOWN:
+				if self.isInt(event.unicode):
+					new_difficulty = int(event.unicode)
+					if new_difficulty > 0 and new_difficulty < 5:
+						self.difficulty = new_difficulty
+						self.loadGame()
+
+
+	def newgame_update(self):
+		pass
+
+	def newgame_draw(self):
+		self.window.fill(WHITE)
+
+		for button in self.newgameButtons:
+			button.draw(self.window)
+
+		pygame.display.update()
+
+
+
 
 ################ PLAYING FUNCTIONS############
 	def playing_events(self):
@@ -67,7 +112,6 @@ class App:
 
 			elif event.type == pygame.KEYDOWN:
 				if self.selected != None:
-
 					if self.isInt(event.unicode) and not self.selected in self.lockedCells:
 						self.grid[self.selected[1]][self.selected[0]] = int(event.unicode)
 						self.cellChanged = True
@@ -140,10 +184,6 @@ class App:
 					# Everything is correct!
 					self.puzzleComplete = True
 
-
-
-
-
 	def playing_draw(self):
 		self.window.fill(WHITE)
 		if self.selected:
@@ -159,12 +199,20 @@ class App:
 		self.drawNumbers(self.window)
 
 		if self.puzzleComplete:
-			textSurface = pygame.font.Font("freesansbold.ttf",20).render("Done!! used {} hints".format(self.hintCounter),True,GREEN)
+			textSurface = pygame.font.Font("freesansbold.ttf",20).render("Done!! used {} hints and took {}".format(self.hintCounter,self.timer),True,GREEN)
 			textRect = textSurface.get_rect()
-			textRect.center  = (380,40)
+			textRect.center  = (340,20)
 			self.window.blit(textSurface,textRect)
 
+
+		textSurface = pygame.font.Font("freesansbold.ttf",20).render("{} - {}".format(difficulty_levels[self.difficulty],self.puzzlenum),True,BLACK)
+		self.window.blit(textSurface,(10,10))
+
+		self.drawTimer(self.window)
+
+
 		pygame.display.update()
+
 
 
 ########### BUTTON FUNCTIONS ##############
@@ -193,6 +241,26 @@ class App:
 			self.grid[self.selected[1]][self.selected[0]] = self.solvedGrid[self.selected[1]][self.selected[0]]
 			self.hintCounter += 1
 
+	def button_new(self,params):
+		print("button_new")
+		self.state = "newgame"
+
+	def button_easy(self,params):
+		print("button_easy")
+		self.difficulty = 1
+		self.loadGame()
+
+	def button_medium(self,params):
+		self.difficulty = 2
+		self.loadGame()
+
+	def button_hard(self,params):
+		self.difficulty = 3
+		self.loadGame()
+
+	def button_evil(self,params):
+		self.difficulty = 4
+		self.loadGame()
 
 
 ########### HELPER FUNCTIONS ###############
@@ -215,18 +283,35 @@ class App:
 			pygame.draw.line(window,BLACK,(gridPos[0],gridPos[1]+(x*cellSize)),(gridPos[0]+450,gridPos[1]+(x*cellSize)),2 if x % 3 == 0 else 1)
 
 
-	def load(self):
-		self.loadButtons()
+	def loadGame(self):
+		self.lockedCells.clear()
+		self.grid = self.getPuzzle(self.difficulty)
+		print(self.grid)
+		self.hintCounter = 0
+		self.puzzleComplete = False
+		for idx,row in enumerate(self.grid):
+			self.solvedGrid[idx]= row.copy()
+		self.solve()
+
 		for yidx,row in enumerate(self.grid):
 			for xidx,num in enumerate(row):
 				if num != 0:
 					self.lockedCells.append([xidx,yidx])
+		self.state = "playing"
+
+		self.init_timer = pygame.time.get_ticks()
 		#print (self.lockedCells)
 
 	def loadButtons(self):
 		self.playingButtons.append(Button(20,40,100,40,text="Clear",color=LIGHTBLUE,highlightcolor=NEONPINK,function=self.button_clear))
-		self.playingButtons.append(Button(480,560,100,40,text="Solve",color=RED,highlightcolor=NEONPINK,function=self.button_solve))
+		self.playingButtons.append(Button(480,560,100,40,text="Give Up",color=RED,highlightcolor=NEONPINK,function=self.button_solve))
 		self.playingButtons.append(Button(140,40,100,40,text="Hint",color=CYAN,highlightcolor=NEONPINK,function=self.button_hint))
+		self.playingButtons.append(Button(480,40,120,40,text="New Game",color=GREEN,highlightcolor=NEONPINK,function=self.button_new))
+
+		self.newgameButtons.append(Button(20,40,100,40,text="(1) Easy",color=GREEN,highlightcolor=NEONPINK,function=self.button_easy))
+		self.newgameButtons.append(Button(20,90,120,40,text="(2) Medium",color=LIGHTBLUE,highlightcolor=NEONPINK,function=self.button_medium))
+		self.newgameButtons.append(Button(20,140,100,40,text="(3) Hard",color=ORANGE,highlightcolor=NEONPINK,function=self.button_hard))
+		self.newgameButtons.append(Button(20,190,100,40,text="(4) Evil",color=RED,highlightcolor=NEONPINK,function=self.button_evil))
 
 
 	def drawNumbers(self,window):
@@ -245,6 +330,26 @@ class App:
 					else:
 						self.textToScreen(window,str(num),pos,color=DARKBLUE)
 
+
+	def drawTimer(self,window):
+		if not self.puzzleComplete:
+			cur_timer = pygame.time.get_ticks()
+			#if (cur_timer % 400) == 0:
+			elapsed = (cur_timer - self.init_timer) / 1000
+			hours = elapsed // 3600
+			mins = elapsed // 60
+			secs = elapsed % 60
+
+			timer = "{:0>2}:{:0>2}:{:0>2}".format(int(hours),int(mins),int(secs))
+			if timer != self.timer:
+				#print(timer)
+				self.timer = timer
+			
+		textSurface = pygame.font.Font("freesansbold.ttf",20).render(self.timer,True,BLACK)
+		window.blit(textSurface,(280,570))
+			
+
+
 	def textToScreen(self,window,text,pos,color=BLACK):
 		font = self.font.render(text,False,color)
 		fontWidth = font.get_width()
@@ -262,8 +367,12 @@ class App:
 		return True
 
 #difficulty is passed in as a string with 1 digit.  1-4
-	def getPuzzle(self,difficulty):
-		html_doc = requests.get("https://nine.websudoku.com/?level={}".format(difficulty)).content()
+	def getPuzzle(self,difficulty,puzzlenum=-1):
+		if puzzlenum == -1:
+			puzzlenum = random.randrange(100)
+		self.puzzlenum = puzzlenum
+		print("puzzle: " + str(self.puzzlenum))
+		return pickle.load(open('puzzles\\{}-{}.pz'.format(difficulty,puzzlenum),'rb'))
 
 
 ####### BOARD CHECKING FUNCTIONS #############
@@ -287,7 +396,6 @@ class App:
 
 	def solve(self):
 		next_cell = self.unfinishedCells(self.solvedGrid)
-		print(next_cell)
 		if next_cell[0] == -1: #all done!
 			return True
 
--- settings.py
+++ settings.py
@@ -10,8 +10,9 @@ GRAY = (150,150,150)
 NEONPINK = (255,0,204)
 CYAN = (0,255,179)
 GREEN = (0,150,0)
+ORANGE = (255,150,0)
 
-testBoard1 = [[0 for x in range(9)] for x in range(9)] 
+testBoard1 = [[0 for x in range(9)] for x in range(9)]
 testBoard2 = [[0,6,0,2,0,0,8,3,1],
 			  [0,0,0,0,8,4,0,0,0],
 			  [0,0,7,6,0,3,0,4,9],
@@ -22,6 +23,7 @@ testBoard2 = [[0,6,0,2,0,0,8,3,1],
 			  [8,1,5,0,2,9,7,0,0],
 			  [0,0,0,0,7,0,0,1,5]]
 
+difficulty_levels = ["None","Easy","Medium","Hard","Evil"]
 
 gridPos = (75,100)
 cellSize = 50
